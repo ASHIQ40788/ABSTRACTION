ff20cd3b16ace25fb460ec04ae35ff1a1cca03a1
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-02 13:46:07 +0530

    ABSTARCTION PROGRANM AND MAINMETHOD

---
+++ ABSTRACTIONProg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSTRACTIONprog
+{
+    abstract class Animal
+    {
+        // Abstract method (does not have a body)
+        public abstract void animalSound();
+        // Regular method
+        public void sleep()
+        {
+            Console.WriteLine("zzzzzzzzzzzzzzzzzzzzzzzzzzz");
+        }
+    }
+
+    // Derived class (inherit from Animal)
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee weeee weee wweeee");
+        }
+    }
+
+}
---
+++ Mainmethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSTRACTIONprog
+{
+    class Mainmethod
+
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig(); // Create a Pig object
+            myPig.animalSound();  // Call the abstract method
+            myPig.sleep();  // Call the regular method
+            Console.Read();
+        }
+    }
+}
